2d8c42e78c2b0043d9d11009408ae28c1a70a1b3
ComputerMagic <bogdan.pogorielow@gmail.com>
2019-03-09 01:46:47 +0300

    push

--- get/get.php
+++ get/get.php
@@ -15,11 +15,8 @@ if($test1==1){
 
 
 $file = 'people.txt';
-// Открываем файл для получения существующего содержимого
 $current = file_get_contents($file);
-// Добавляем  в файл
 $current .= "TEST1\n";
-// Пишем содержимое обратно в файл
 file_put_contents($file, $current);
 
 
@@ -32,11 +29,8 @@ file_put_contents($file, $current);
 
 }else if($test1==2){
 $file = 'people.txt';
-// Открываем файл для получения существующего содержимого
 $current = file_get_contents($file);
-// Добавляем  в файл
 $current .= "Aleks Sviridov\n";
-// Пишем содержимое обратно в файл
 file_put_contents($file, $current);
 
 
@@ -44,11 +38,8 @@ file_put_contents($file, $current);
 
 
 $file = 'people.txt';
-// Открываем файл для получения существующего содержимого
 $current = file_get_contents($file);
-// Добавляем в  файл
 $current .= $test1;
-// Пишем содержимое обратно в файл
 file_put_contents($file, $current);
 
 }
