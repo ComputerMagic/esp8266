d6c269ba498bbb913984a0518f0981ccddb577e6
ComputerMagic <bogdan.pogorielow@gmail.com>
2019-03-09 02:02:21 +0300

    push

--- get/get.ino
+++ get/get.ino
@@ -3,9 +3,10 @@
  
 const char* ssid     = "royal-kosmo";
 const char* password = "0978558521";
- const char* host = "smart-home.in.ua";
- 
+const char* host = "smart-home.in.ua";
 
+char[] property;
+char[] value;
 void setup() {
 
   WiFi.mode(WIFI_STA);
@@ -14,30 +15,29 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
- 
   }
 }
-void loop()
-{
 
- 
+void loop()
+{ 
   WiFiClient client;
  
+
+}
+
+
+void GETprint(property, value){
+  char[] print = property + "=" + value;
   if (client.connect(host, 80))
     {
     client.print( "GET /get.php?");
-    client.print("test=privet");
-
-    
+    client.print(print);
     client.println( " HTTP/1.1");
     client.print( "Host:" );
     client.println(host);
     client.println( "Connection: close" );
-        client.println();
-        client.println();
-
-   delay(10000); 
-
-}
-
+    client.println();
+    client.println();
+  }
+  else Serial.println("ERROR client print");
 }
